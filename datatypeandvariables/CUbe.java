d94a8756baacc7ea371c2796f33b9630c4c219d1
Nagesh mane <131618139+Nageshmane@users.noreply.github.com>
2023-05-04 16:30:38 +0530

    Add files via upload

---
+++ datatypeandvariables/CUbe.java
@@ -0,0 +1,7 @@
+public class CUbe {
+    public static void main(String[] args) {
+        int n=4;
+        int cube=n*n*n;
+        System.out.println(cube);
+    }
+}
---
+++ datatypeandvariables/Swap.java
@@ -0,0 +1,14 @@
+ 
+public class Swap {  
+    public static void main(String[] args) {  
+       int x=2, y=3, t;// x and y are to swap   
+       
+       System.out.println("before swapping numbers: "+x +"  "+ y);  
+       /*swapping */  
+       t = x;  
+       x = y;  
+       y = t;  
+       System.out.println("After swapping: "+x +"   " + y);  
+       System.out.println( );  
+    }    
+}  
